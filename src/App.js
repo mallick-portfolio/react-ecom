489741515a581f432823ef521396b1b51d5a9eb0
mallick-portfolio <tamal@gmail.com>
2022-04-12 16:09:13 +0600

    added home page slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Header from "./Pages/Shared/Header";
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Latest from './Latest/Latest'
+import Slider from './Slider/Slider'
+
+const Home = () => {
+  return (
+    <div>
+      <Slider />
+      <Latest />
+    </div>
+  )
+}
+
+export default Home
