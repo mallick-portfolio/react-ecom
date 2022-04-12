680f645b822e799f17ea7a3f1cd2d26db5346491
mallick-portfolio <tamal@gmail.com>
2022-04-12 22:59:11 +0600

    updeted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Cart from "./Pages/Cart/Cart";
 import Details from "./Pages/Details/Details";
 import Home from "./Pages/Home/Home";
 import Header from "./Pages/Shared/Header";
@@ -10,7 +11,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/details/:id" element={<Details />} />
+
       </Routes>
     </div>
   );
--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,18 +1,22 @@
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+
+
 const Header = () => {
+
+  const state = useSelector((state)=> state.handleCart)
+  console.log(state)
   return (
     <nav className="bg-slate-300 border-gray-200 px-2 sm:px-4 py-5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com" className="flex items-center">
+        <Link to="/" className="flex items-center">
           {/* <img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/> */}
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            ESHOP
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="mobile-menu"
           type="button"
@@ -20,7 +24,31 @@ const Header = () => {
           aria-controls="mobile-menu"
           aria-expanded="false"
         >
-          <FontAwesomeIcon icon={faHome} />
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
@@ -42,6 +70,11 @@ const Header = () => {
             <li>
               <Link to="/" className="nav-item">
                 Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className="nav-item">
+                Cart {state ? state.length : 0}
               </Link>
             </li>
           </ul>
